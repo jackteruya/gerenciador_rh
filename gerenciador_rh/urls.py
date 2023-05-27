b70c39fee3dc264330198dfb0de013e654cdc58c
jackteruya <jack.teruya@gmail.com>
2023-05-26 20:37:43 -0400

    add jwt

--- gerenciador_rh/urls.py
+++ gerenciador_rh/urls.py
@@ -16,11 +16,15 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from django.views import static as url_static
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -37,4 +41,9 @@ urlpatterns = [
     re_path(r"^media/(?P<path>.*)$", url_static.serve, {"document_root": settings.MEDIA_ROOT}),
     path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('', include('empresas.urls')),
 ]
